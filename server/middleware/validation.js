1c9b47a03522eabab2e509b0031d966e6f0288bf
james <james.coates@me.com>
2019-10-12 17:14:45 +0100

    add seperate validation middleware for edit user

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,8 +1,14 @@
 const { check } = require('express-validator');
 
-module.exports.userDetails = [
+module.exports.addUserDetails = [
   check('username', 'Username required').isLength({ min: 5 }),
   check('username', 'Username must contain alphanumeric characters only').isAlphanumeric(),
   check('password', 'Password is required').not().isEmpty(),
+  check('email', 'Email does not appear to be valid').isEmail(),
+];
+
+module.exports.editUserDetails = [
+  check('username', 'Username required').isLength({ min: 5 }),
+  check('username', 'Username must contain alphanumeric characters only').isAlphanumeric(),
   check('email', 'Email does not appear to be valid').isEmail(),
 ];
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,14 +3,15 @@ const passport = require('passport');
 
 const userController = require('../controllers/users');
 const validationMiddleware = require('../middleware/validation');
+
 const authentication = passport.authenticate('jwt', { session: false });
 
 const router = express.Router();
 
 router.get('/', userController.getUsers);
 router.get('/:username', authentication, userController.getUser);
-router.post('/', validationMiddleware.userDetails, authentication, userController.addUser);
-router.put('/:username', validationMiddleware.userDetails, authentication, userController.editUser);
+router.post('/', validationMiddleware.addUserDetails, authentication, userController.addUser);
+router.put('/:username', validationMiddleware.editUserDetails, authentication, userController.editUser);
 router.post('/:username/movies/:title', authentication, userController.addFavouriteMovie);
 router.delete('/:username', authentication, userController.deleteUser);
 
