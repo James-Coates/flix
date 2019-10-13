a02045fe2a73e36daf33d9ecb454828d7918bc6a
james <james.coates@me.com>
2019-10-13 11:28:41 +0100

    deploy test

--- server/routes/client.js
+++ server/routes/client.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   console.log('123');
   console.log(path.resolve(__dirname, '../../client', 'dist', 'index.html'));
+  console.log(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  console.log(__dirname);
   res.sendFile(path.join(path.resolve(__dirname, '../../client', 'dist', 'index.html')));
 });
 
