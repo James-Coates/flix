6e1fb5ab9373fc8011c5c937832576efe7f6457c
james <james.coates@me.com>
2019-10-13 11:37:07 +0100

    change prod api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://flix-db.herokuapp.com';
+export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://flix-db.herokuapp.com/api';
 
 export const setHeaderOptions = (token) => ({
   headers: {
